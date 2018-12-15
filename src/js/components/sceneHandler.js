1133ce2b970a24dcddc80669daa6bcdc0cbc1b28
LauraWyffels <laura.wyffels@student.howest.be>
2018-12-15 21:37:41 +0100

    made some changes to explosion

--- src/js/components/sceneHandler.js
+++ src/js/components/sceneHandler.js
@@ -1,14 +1,10 @@
 AFRAME.registerComponent(`scenehandler`, {
-  init() {
-    this.el.addEventListener(`click`, () => {
-      window.location = `index2.html`;
-      // if (!playing) {
-      //     scene.play();
-      // } else {
-      //     scene.pause();
-      //     scene.currentTime = 0;
-      // }
-      // playing = !playing;
-    });
-  }
+    init() {
+        this.el.addEventListener(`click`, () => {
+            if (window.location = `index.html`) {
+                window.location = `disarmBomb.html`;
+            }
+
+        });
+    }
 });
