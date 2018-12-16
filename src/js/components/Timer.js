d65e8e3b4cf71d0e9b0f6415be4ef5d2d64d3999
LauraWyffels <laura.wyffels@student.howest.be>
2018-12-17 00:04:28 +0100

    fixed bug position

--- src/js/components/Timer.js
+++ src/js/components/Timer.js
@@ -2,41 +2,41 @@ let gameOver = false;
 
 AFRAME.registerComponent(`timer`, {
     init() {
-        // const randomAmountOfMinutes = Math.floor(Math.random() * (3 - 1)) + 1;
-        const randomAmountOfMinutes = .1;
+        const randomAmountOfMinutes = Math.floor(Math.random() * (3 - 1)) + 1;
         const counter = document.querySelector(`.timer`);
 
         let resterendeTijd = (randomAmountOfMinutes * 60);
 
-        setInterval( () => {
+
+        setInterval(() => {
             resterendeTijd -= 1;
             const m = Math.floor(resterendeTijd / 60);
             const s = Math.floor(resterendeTijd % 60);
             counter.setAttribute(`value`, `${m < 10 ? `0` : ``}${m.toString()}:${s < 10 ? `0` : ``}${s.toString()}`);
 
-            if(resterendeTijd <= 0) {
+            if (resterendeTijd <= 0) {
                 gameOver = true;
                 clearInterval();
                 counter.setAttribute(`value`, `00:00`);
                 counter.setAttribute(`template`, `src: #blink`);
-                
+
                 // crash plane
                 const planeParts = [...document.querySelectorAll(`.plane-part`)];
                 const sky = document.querySelector(`.sky`);
                 const bomb = document.querySelector(`.bomb`);
-                
+
                 [...planeParts, sky, bomb].forEach(part => {
                     part.setAttribute(`template`, `src: #crashAnimation`);
                 });
-                
+
                 // remove all pop-ups
                 [...document.querySelectorAll(`.popup-text`)].forEach(popup => popup.setAttribute(`visible`, `false`));
-                
+
             }
-        }, 1000);   
+        }, 1000);
     },
     tick() {
-        if(gameOver) {
+        if (gameOver) {
             const $audio = document.getElementById(`bombSound`);
             $audio.play();
             const $audio2 = document.getElementById(`crashSound`);
