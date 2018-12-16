3ff169b85c34956798f6d2de1090def320f542e5
LauraWyffels <laura.wyffels@student.howest.be>
2018-12-16 15:05:24 +0100

    walk to next scene

--- src/js/components/sceneHandler.js
+++ src/js/components/sceneHandler.js
@@ -1,35 +1,35 @@
-AFRAME.registerComponent(`scenehandler`, {
-  schema: {
-    visible: { default: `false` }
-  },
-  init() {
-    const data = this.data;
+// AFRAME.registerComponent(`scenehandler`, {
+//   schema: {
+//     visible: { default: `false` }
+//   },
+//   init() {
+//     const data = this.data;
 
-    // popupText1
+//     // popupText1
 
-    const $popUp = document.getElementById(`popupText1`);
-    const $disarm = document.getElementById(`disarm`);
-    const $getOut = document.getElementById(`getOut`);
+//     const $popUp = document.getElementById(`popupText1`);
+//     const $disarm = document.getElementById(`disarm`);
+//     const $getOut = document.getElementById(`getOut`);
 
-    $disarm.addEventListener(`click`, () => {
-      $popUp.setAttribute(`visible`, data.visible);
-    });
-    $getOut.addEventListener(`click`, () => {
-      $popUp.setAttribute(`visible`, data.visible);
-    });
+//     $disarm.addEventListener(`click`, () => {
+//       $popUp.setAttribute(`visible`, data.visible);
+//     });
+//     $getOut.addEventListener(`click`, () => {
+//       $popUp.setAttribute(`visible`, data.visible);
+//     });
 
-    // popupText2
+//     // popupText2
 
-    const $popUp2 = document.getElementById(`popupText2`);
-    const $redWire = document.getElementById(`redWire`);
-    const $blueWire = document.getElementById(`blueWire`);
+//     // const $popUp2 = document.getElementById(`popupText2`);
+//     // const $redWire = document.getElementById(`redWire`);
+//     // const $blueWire = document.getElementById(`blueWire`);
 
-    $redWire.addEventListener(`click`, () => {
-      $popUp2.setAttribute(`visible`, data.visible);
-    });
-    $blueWire.addEventListener(`click`, () => {
-      $popUp2.setAttribute(`visible`, data.visible);
-    });
+//     // $redWire.addEventListener(`click`, () => {
+//     //   $popUp2.setAttribute(`visible`, data.visible);
+//     // });
+//     // $blueWire.addEventListener(`click`, () => {
+//     //   $popUp2.setAttribute(`visible`, data.visible);
+//     // });
 
-  }
-});
+//   }
+// });
