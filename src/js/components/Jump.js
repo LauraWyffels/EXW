0ea67619978dbd9018c277a0b635ba7f05daf45b
Sjouwke <sjouwke.van.parys@student.howest.be>
2018-12-16 23:24:46 +0100

    added crash to blueWire; made crash better

--- src/js/components/Jump.js
+++ src/js/components/Jump.js
@@ -9,10 +9,7 @@ AFRAME.registerComponent(`jump`, {
 });
 
 const handleClickJump = () => {
-
     const fallingObjects = [...document.querySelectorAll(`.falling-object`)];
-
-
     fallingObjects.forEach(obj => obj.setAttribute(`template`, `src: #walkFrontAnimation`));
     setTimeout(() => {
         fallingObjects.forEach(obj => obj.setAttribute(`template`, `src: #walkLeftAnimation`));
@@ -22,7 +19,6 @@ const handleClickJump = () => {
                 const $audio = document.getElementById(`aaaah`);
                 $audio.play();
                 setTimeout(() => {
-                    const data = this.data;
                     const $sky = document.querySelector(`.sky`);
                     $sky.setAttribute(`visible`, data.visible);
                 }, 6000)
--- src/js/components/Timer.js
+++ src/js/components/Timer.js
@@ -1,6 +1,6 @@
 AFRAME.registerComponent(`timer`, {
     init() {
-        const randomAmountOfMinutes = Math.floor(Math.random() * (6 - 3)) + 3;
+        const randomAmountOfMinutes = Math.floor(Math.random() * (2 - 1)) + 1;
         const counter = document.querySelector(`.timer`);
 
         let resterendeTijd = (randomAmountOfMinutes * 60);
@@ -15,8 +15,19 @@ AFRAME.registerComponent(`timer`, {
                 clearInterval();
                 counter.setAttribute(`value`, `00:00`);
                 counter.setAttribute(`template`, `src: #blink`);
+                
+                // crash plane
+                const planeParts = [...document.querySelectorAll(`.plane-part`)];
+                const sky = document.querySelector(`.sky`);
+                const bomb = document.querySelector(`.bomb`);
+                
+                [...planeParts, sky, bomb].forEach(part => {
+                    part.setAttribute(`template`, `src: #crashAnimation`);
+                });
 
-                // STOP GAME
+                // remove all pop-ups
+                [...document.querySelectorAll(`.popup-text`)].forEach(popup => popup.setAttribute(`visible`, `false`));
+                
             }
         }, 1000);   
     }
