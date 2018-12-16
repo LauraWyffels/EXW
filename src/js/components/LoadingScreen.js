80ac2ba41178874130bd6e351802b83a29f9bc58
Sjouwke <sjouwke.van.parys@student.howest.be>
2018-12-16 11:21:02 +0100

    working on loading screen

--- src/js/components/LoadingScreen.js
+++ src/js/components/LoadingScreen.js
@@ -1,6 +1,6 @@
 AFRAME.registerComponent(`loading-screen`, {
   init() {
-    const $el = this.el;  // = box or sphere or...
+    const $el = this.el;  
     setTimeout(() => {
       $el.parentElement.removeChild($el);
     }, 15000);
