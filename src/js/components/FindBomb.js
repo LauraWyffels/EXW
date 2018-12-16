3fe7186993e8c11658f4f2fff71f689e6f8c57d5
LauraWyffels <laura.wyffels@student.howest.be>
2018-12-16 23:36:33 +0100

    landing button, red wire -> sit

--- src/js/components/FindBomb.js
+++ src/js/components/FindBomb.js
@@ -1,11 +1,11 @@
 AFRAME.registerComponent(`findbomb`, {
     init() {
-        const $el = this.el; 
-        $el.addEventListener(`click`, handleClickDisarm);
+        const $el = this.el;
+        $el.addEventListener(`click`, handleClickFind);
     }
 });
 
-const handleClickDisarm = () => {
+const handleClickFind = () => {
     const fallingObjects = [...document.querySelectorAll(`.falling-object`)];
     fallingObjects.forEach(obj => obj.setAttribute(`template`, `src: #walkToCockPitAnimation`));
 }
