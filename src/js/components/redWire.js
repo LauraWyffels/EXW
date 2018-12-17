8c7f30d4a8d0bb32c48312dabc2a073fb78d3776
LauraWyffels <laura.wyffels@student.howest.be>
2018-12-17 01:47:39 +0100

    added buttons to land or crash plane

--- src/js/components/redWire.js
+++ src/js/components/redWire.js
@@ -5,21 +5,23 @@ AFRAME.registerComponent(`redwire`, {
     init() {
         const data = this.data;
         const $button = document.querySelector(`.redWireButton`);
-        const $bomb = document.querySelector(`.bomb`);
+
 
         $button.addEventListener(`click`, () => {
-            // $bomb.setAttribute(`visible`, data.visible);
-
-            // const fallingObjects = [...document.querySelectorAll(`.falling-object`)];
-            // fallingObjects.forEach(obj => obj.setAttribute(`template`, `src: #sitInCockPitAnimation`));
-
-            const planeParts = [...document.querySelectorAll(`.plane-part`)];
-            const bomb = document.querySelector(`.bomb`);
+            const $bomb = document.querySelector(`.bomb`);
             $bomb.setAttribute(`visible`, data.visible);
 
-            [...planeParts, bomb].forEach(part => {
-                part.setAttribute(`template`, `src: #sitInCockPitAnimation`);
-            });
+            const fallingObjects = [...document.querySelectorAll(`.falling-object`)];
+            fallingObjects.forEach(obj => obj.setAttribute(`template`, `src: #sitInCockPitAnimation`));
+
+            // const planeParts = [...document.querySelectorAll(`.plane-part`)];
+            // const bomb = document.querySelector(`.bomb`);
+            // const crashButton = document.querySelector(`.crash-button`);
+            // $bomb.setAttribute(`visible`, data.visible);
+
+            // [...planeParts, bomb, crashButton].forEach(part => {
+            //     part.setAttribute(`template`, `src: #sitInCockPitAnimation`);
+            // });
         });
     }
 });
